45311744fbe6b150ac65560a41129cb136c186a2
winnerkobe <hsiehziping@gmail.com>
2025-04-17 21:22:52 +0800

    Add new file

---
+++ 0415.py
@@ -0,0 +1,9 @@
+name = input('what is your name?')
+print('Hi,', name)
+age = int(input('What is your age?'))-20
+print('you are',age,'years old')
+height = input('What is your height?')
+weight = float(input('What is your weight?'))+20
+print('Your height is',height, 'Your weight is', weight,'KG, a bit too fat')
+print(f"Your height is {height}.Your weight is{weight}kg a bit too fat")
+print('goodbye,', name)
